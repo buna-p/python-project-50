8b5df318e12e044cbc5d29379b45d9c98b6f9f4f
Natali <burdinsckaya@ya.ru>
2026-01-27 09:30:24 +0300

    add gendiff

---
+++ gendiff/scripts/argparser.py
@@ -0,0 +1,10 @@
+from argparse import ArgumentParser
+
+
+def parse_args():
+    argparser = ArgumentParser(description='Compares two configuration files and shows a difference.') # noqa: E501
+    argparser.add_argument('first_file', type=str)
+    argparser.add_argument('second_file', type=str)
+    args = argparser.parse_args()
+    print(f'{args.first_file} {args.second_file}')
+    return args.first_file, args.second_file
---
+++ gendiff/scripts/gendiff.py
@@ -0,0 +1,8 @@
+from .argparser import parse_args
+
+def main():
+    parse_args()
+
+
+if __name__ == "__main__":
+    main()
