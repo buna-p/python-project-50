6835e9dff0b32608e99cff9179ec236538afc267
Natali <burdinsckaya@ya.ru>
2026-02-08 22:47:53 +0300

    add find_diff.py

--- gendiff/scripts/find_diff.py
+++ gendiff/scripts/find_diff.py
@@ -4,13 +4,35 @@ def find_diff(data1: dict, data2: dict) -> str:
 
     for key in keys:
         if key in data1 and key in data2:
-            if data1[key] == data2[key]:
-                diff.append(f'  {key}: {data1[key]}')
+            if isinstance(data1[key], dict) and isinstance(data2[key], dict):
+                diff.append({
+                        'key': key,
+                        'operation': '[complex value]',
+                        'childrens': find_diff(data1[key], data2[key])
+                    })
+            elif data1[key] == data2[key]:
+                diff.append({
+                    'key': key,
+                    'operation': 'not changed',
+                    'value': data1[key]
+                })
             else:
-                diff.append(f'- {key}: {data1[key]}')
-                diff.append(f'+ {key}: {data2[key]}')
+                diff.append({
+                    'key': key,
+                    'operation': 'updated',
+                    'value old': data1[key],
+                    'value new': data2[key],
+                })
         elif key in data1:
-            diff.append(f'- {key}: {data1[key]}')
+            diff.append({
+                    'key': key,
+                    'operation': 'removed',
+                    'value': data1[key]
+                })
         else:
-            diff.append(f'+ {key}: {data2[key]}')
-    return '\n'.join(diff)
+            diff.append({
+                    'key': key,
+                    'operation': 'added',
+                    'value': data2[key]
+                })
+    return diff
