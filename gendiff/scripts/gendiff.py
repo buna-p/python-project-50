daa91f0a34111148c6e4f77e68265e9a4836123b
Natali <burdinsckaya@ya.ru>
2026-02-09 00:41:10 +0300

    upd gendiff.py

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -2,6 +2,7 @@ from .argparser import parse_arguments
 from gendiff.scripts.find_diff import find_diff
 from gendiff.scripts.converter import converter
 from gendiff.scripts.formattes.stylish import stylish
+from gendiff.scripts.formattes.plain import plain
 
 def generate_diff(file_path1: str, file_path2: str, format_name='stylish') -> str:
     data1 = converter(file_path1)
@@ -9,6 +10,8 @@ def generate_diff(file_path1: str, file_path2: str, format_name='stylish') -> st
     diff = find_diff(data1, data2)
     if format_name == 'stylish':
         formated_diff = stylish(diff)
+    elif format_name == 'plain':
+        formated_diff = plain(diff)
     return formated_diff
 
 
