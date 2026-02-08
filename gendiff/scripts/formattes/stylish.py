f852505ab38df20e014b6c482668374e18cf2138
Natali <burdinsckaya@ya.ru>
2026-02-08 22:46:00 +0300

    add formattes

---
+++ gendiff/scripts/formattes/stylish.py
@@ -0,0 +1,61 @@
+INDENT = 4
+SHIFT = 2
+
+
+def format_value(value, depth) -> str:
+    if isinstance(value, bool):
+        return str(value).lower()
+    elif isinstance(value, dict):
+        result = ['{']
+        indent = ' ' * INDENT * (depth + 1)
+        for k, v in value.items():
+            result.append(f'{indent}{k}: {format_value(v, depth + 1)}')
+        result.append(f'{(' ' * INDENT * depth)}}}')
+        return '\n'.join(result)
+    elif value == '':
+        return ''
+    elif not value:
+        return 'null'
+    else:
+        return value
+
+
+def stylish(diff: dict, depth=1) -> str:
+    indent = ' ' * INDENT * depth
+    indent_with_shift = ' ' * (INDENT * depth - SHIFT)
+    result = ['{']
+    for data in diff:
+        if 'childrens' in data:
+            childrens = stylish(data['childrens'], depth + 1)
+            result.append(f'{indent}{data['key']}: {childrens}')
+        elif data['operation'] == 'not changed':
+            result.append(
+                f'{indent}{data['key']}: {
+                    format_value(data['value'], depth)
+                    }'
+                )
+        elif data['operation'] == 'updated':
+            result.append(
+                f'{indent_with_shift}- {data['key']}: {
+                    format_value(data['value old'], depth)
+                    }'
+                )
+            result.append(
+                f'{indent_with_shift}+ {data['key']}: {
+                    format_value(data['value new'], depth)
+                    }'
+                )
+        elif data['operation'] == 'removed':
+            result.append(
+                f'{indent_with_shift}- {data['key']}: {
+                    format_value(data['value'], depth)
+                    }'
+                )
+        else:
+            result.append(
+                f'{indent_with_shift}+ {data['key']}: {
+                    format_value(data['value'], depth)
+                    }'
+                )
+    result.append(f'{(' ' * INDENT * (depth - 1))}}}')
+    return '\n'.join(result)
