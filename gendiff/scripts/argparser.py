821db8c06bb26317936ef3cade72e496b49c9673
Natali <burdinsckaya@ya.ru>
2026-02-09 12:02:06 +0300

    update argparser

--- gendiff/scripts/argparser.py
+++ gendiff/scripts/argparser.py
@@ -1,11 +1,11 @@
 from argparse import ArgumentParser
 
 
-def parse_args():
-    argparser = ArgumentParser(description='Compares two configuration files and shows a difference.') # noqa: E501
+def parse_arguments():
+    argparser = ArgumentParser(description='Compares two configuration files and shows a difference.')  # noqa: E501
     argparser.add_argument('first_file', type=str)
     argparser.add_argument('second_file', type=str)
-    argparser.add_argument('-f', '--format', type=str)
+    argparser.add_argument('-f', '--format', default='stylish', choices=['stylish', 'plain', 'json'], help='set format of output (default: "stylish")', type=str)  # noqa: E501
     args = argparser.parse_args()
     print(f'{args.first_file} {args.second_file}')
     return args.first_file, args.second_file
